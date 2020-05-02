e14a6b15c2b746c947938324173867d438e4bbfa
Denis BOUVET <denis.bouvet@live.com>
2020-05-02 11:29:21 +0200

    Ajoutez des fichiers projet.

---
+++ Xamarin Simple Clock/Xamarin Simple Clock/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_Simple_Clock
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(10), () =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    lblclock.Text = DateTime.Now.ToString("hh:mm:ss:f");
+                });
+                return true;
+            });
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            lblresult.Text = string.Concat(lblresult.Text, lblclock.Text, Environment.NewLine);
+
+        }
+    }
+}
